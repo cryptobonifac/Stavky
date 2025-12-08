021971a63bd0df1d3d7c9840423528b05c939ad1
mro10 <maro10@protonmail.com>
2025-12-08 11:15:52 +0100

    chore: Enforce strict TypeScript checks in development and update configuration for ESLint integration

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const withNextIntl = require('next-intl/plugin')('./i18n/request.ts')
 
 const nextConfig = {
   reactStrictMode: true,
+  // Enforce strict type checking in development mode
+  typescript: {
+    // Don't ignore build errors - fail on TypeScript errors
+    ignoreBuildErrors: false,
+  },
+  // Note: ESLint is configured via eslint.config.js or .eslintrc.json
+  // Next.js 16 runs ESLint by default during build and fails on errors
 }
 
 module.exports = withNextIntl(nextConfig)
